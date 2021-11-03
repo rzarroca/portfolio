a8c8486240c1999e1eb0bd6590c990a2d2ed457e
ricardozarroca <ricardozarroca@gmail.com>
2021-11-03 10:07:45 +0000

    Landing Page Created and added to App

---
+++ src/pages/landingPage/Ladingpage.js
@@ -0,0 +1,9 @@
+import { FullPage } from '../../components/FullPage/FullPage'
+
+export function LadingPage () {
+  return (
+    <FullPage>
+      <h1>Hola que tal vamos que funciona</h1>
+    </FullPage>
+  )
+}
