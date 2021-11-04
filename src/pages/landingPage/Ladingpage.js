6f65066eae4b5387555e5dc12a5ce080a4b5782b
ricardozarroca <ricardozarroca@gmail.com>
2021-11-04 15:45:09 +0000

    Summary: grid set

--- src/pages/landingPage/Ladingpage.js
+++ src/pages/landingPage/Ladingpage.js
@@ -6,16 +6,19 @@ import avatar from '../../assets/images/DSC04573.JPG'
 export function LadingPage () {
   return (
     <FullPage primary>
-      <div className='fullpage-content'>
+      <div className='landingPage'>
         <article className='summary'>
           <header>
             <h1>Hello World!</h1>
             <h2>I'm Ricardo Zarroca, <bold>fullstack web developer</bold></h2>
           </header>
           <figure>
-            <img src={avatar} alt='avatar image' width='300px' height='300px' />
+            <img src={avatar} alt='avatar image' />
           </figure>
-          <p>Every time the human being has had to solve a need, he has found a solution <bold>based on technology.</bold>
+          <p className='p1'>
+            Every time the human being has had to solve a need, he has found a solution <bold>based on technology.</bold>
+          </p>
+          <p className='p2'>
             This is one of the motivations that have driven me to enter the world of web development and technology, which offers <bold>endless opportunities</bold> to solve our needs and push us to a new level.
           </p>
         </article>
