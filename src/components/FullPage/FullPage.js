2d86f129d2e2fb756c68a0224c5e3fa4c1a1ec51
ricardozarroca <ricardozarroca@gmail.com>
2021-11-05 18:26:19 +0000

    Component Fullpage: props drilling now allowed

--- src/components/FullPage/FullPage.js
+++ src/components/FullPage/FullPage.js
@@ -1,11 +1,11 @@
 import './fullpage.css'
 import { getColor } from 'helpers/getColor'
 
-export function FullPage ({ children, background }) {
+export function FullPage ({ children, background, ...props }) {
   const backgroundColor = background ? `fullpage-${getColor(background)}` : 'fullpage-dark'
 
   return (
-    <section className={`fullpage ${backgroundColor}`}>
+    <section className={`fullpage ${backgroundColor}`} {...props}>
       {children}
     </section>
 
