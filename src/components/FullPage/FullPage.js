839db0051ac1df6ea91a0c96d3da323ac3375f1d
Ricardo Zarroca <ricardozarroca@gmail.com>
2021-11-17 18:11:27 +0000

    Component FullPage: Added Css to make heigth fluid

--- src/components/FullPage/FullPage.js
+++ src/components/FullPage/FullPage.js
@@ -1,11 +1,12 @@
 import './fullpage.css'
 import { getColor } from 'helpers/getColor'
 
-export function FullPage ({ children, background, ...props }) {
+export function FullPage ({ children, background, extend, ...props }) {
   const backgroundColor = background ? `fullpage-${getColor(background)}` : 'fullpage-dark'
+  const extendDisplay = extend ? 'fullpage-extend' : ''
 
   return (
-    <section className={`fullpage ${backgroundColor}`} {...props}>
+    <section className={`fullpage ${backgroundColor} ${extendDisplay}`} {...props}>
       {children}
     </section>
 
