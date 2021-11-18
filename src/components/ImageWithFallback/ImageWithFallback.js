0ba66d2b3566710007443fad54b1be01b983ad97
Ricardo Zarroca <ricardozarroca@gmail.com>
2021-11-18 18:32:52 +0000

    Component Image: Added props with spread operator
    This will allow add class to return img, so you can modify css

--- src/components/ImageWithFallback/ImageWithFallback.js
+++ src/components/ImageWithFallback/ImageWithFallback.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import fallback from 'assets/svg/fallback-image.svg'
 
-export function ImageWithFallback ({ image, alt, fallbackColor, fallbackSrc = fallback }) {
-  const [imageError, setImageError] = useState(false)
+export function ImageWithFallback ({ image, alt, fallbackSrc = fallback, ...props }) {
+  // eslint-disable-next-line prefer-const
+  let [imageError, setImageError] = useState(false)
   return (
     <img
-      src={imageError ? image : fallbackSrc}
+      src={!imageError ? image : fallbackSrc}
       alt={alt}
       onError={() => setImageError(true)}
+      {...props}
     />
   )
 }
