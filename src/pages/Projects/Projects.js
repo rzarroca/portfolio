4c82769846187190c38929194687099b61fcfa4a
Ricardo Zarroca <ricardozarroca@gmail.com>
2021-11-19 18:27:08 +0000

    Page Projects: Added Links to other links

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,6 +3,8 @@ import './projects.css'
 import { FullPage } from 'components/FullPage/FullPage'
 import { CardProject } from 'components/CardProject/CardProject'
 import { CardProjectLinks } from 'components/CardProjectLinks/CardProjectLinks'
+import { Navlist } from 'components/NavList/NavList'
+import { Link } from 'components/Link/Link'
 
 import WebAppImg from 'assets/images/webApp.jpg'
 import PortfolioImg from 'assets/images/portfolio.jpg'
@@ -40,18 +42,11 @@ export function Projects () {
           </CardProject>
         </section>
 
-        <section className='project-section'>
-          <h1 className='project-title'>Other Projects</h1>
-          <nav>
-            <ul>
-              <li><a href='#'>Project</a></li>
-              <li><a href='#'>Project</a></li>
-              <li><a href='#'>Project</a></li>
-              <li><a href='#'>Project</a></li>
-              <li><a href='#'>Project</a></li>
-            </ul>
-          </nav>
-        </section>
+        <Navlist title='Other Projects' color='clear'>
+          <Link small color='clear'>Pokemon app - React practise</Link>
+          <Link href='https://github.com/rzarroca/react_template' small color='clear'>React webpack template repository</Link>
+          <Link href='https://rzarroca.github.io/etch-a-skecth/' small color='clear'>Etch a sketch - Vanilla JS practise</Link>
+        </Navlist>
       </div>
     </FullPage>
   )
