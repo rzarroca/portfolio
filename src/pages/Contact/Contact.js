62f346817e2b9102798491297f0673367b06a103
Ricardo Zarroca <ricardozarroca@gmail.com>
2021-11-21 02:18:54 +0000

    Page Contact: Improved maintanability

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-indent */
 import './contact.css'
+import { v4 as uuid } from 'uuid'
 import { Link } from 'components/Link/Link'
 
 import { ReactComponent as Mail } from 'assets/svg/mail.svg'
@@ -8,30 +10,54 @@ import { ReactComponent as Linkedin } from 'assets/svg/linkedin.svg'
 import { ReactComponent as GitHub } from 'assets/svg/github.svg'
 
 export function Contact () {
+  const listArray = [
+    {
+      type: 'link',
+      href: 'mailto: ricardozarroca@gmail.com',
+      text: 'ricardozarroca@gmail.com',
+      icon: <Mail fill='#E8C084' className='sideLinks-icon' />
+    },
+    {
+      type: 'link',
+      href: 'tel:+34633670474',
+      text: '+34-633670474',
+      icon: <Phone fill='#E8C084' className='sideLinks-icon' />
+    },
+    {
+      type: 'text',
+      text: 'Las Palmas de Gran Canaria, Spain',
+      icon: <Pin fill='#E8C084' className='sideLinks-icon' />
+    },
+    {
+      type: 'link',
+      href: 'https://linkedin.com/in/rzarroca',
+      text: 'https://linkedin.com/in/rzarroca',
+      icon: <Linkedin fill='#E8C084' className='sideLinks-icon' />
+    },
+    {
+      type: 'link',
+      href: 'https://github.com/rzarroca',
+      text: 'github.com/rzarroca',
+      icon: <GitHub fill='#E8C084' className='sideLinks-icon' />
+    }
+  ]
   return (
     <aside className='sideLinks'>
       <h2>Contact Info</h2>
       <ul className='sideLinks-list'>
-        <li className='sideLinks-item'>
-          <Link href='mailto: ricardozarroca@gmail.com' small>ricardozarroca@gmail.com </Link>
-          <Mail fill='#E8C084' className='sideLinks-icon' />
-        </li>
-        <li className='sideLinks-item'>
-          <Link href='tel:+34633670474' small>+34-633670474</Link>
-          <Phone fill='#E8C084' className='sideLinks-icon' />
-        </li>
-        <li className='sideLinks-item'>
-          Las Palmas de Gran Canaria, Spain
-          <Pin fill='#E8C084' className='sideLinks-icon' />
-        </li>
-        <li className='sideLinks-item'>
-          <Link href='https://linkedin.com/in/rzarroca' small>linkedin.com/in/rzarroca</Link>
-          <Linkedin fill='#E8C084' className='sideLinks-icon' />
-        </li>
-        <li className='sideLinks-item'>
-          <Link href='https://github.com/rzarroca' small>github.com/rzarroca</Link>
-          <GitHub fill='#E8C084' className='sideLinks-icon' />
-        </li>
+        {listArray.map(item => (
+          <li key={uuid()} className='sideLinks-item'>
+            {item.type === 'link'
+              ? <>
+                  <Link href={item.href} small color='dark'>{item.text}</Link>
+                  {item.icon}
+                </>
+              : <>
+                  {item.text}
+                  {item.icon}
+                </>}
+          </li>
+        ))}
       </ul>
     </aside>
   )
